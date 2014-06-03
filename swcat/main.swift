4da3c789b0f6ff7905bc77ac907e76f34fa190cc
Caius Durling <dev@caius.name>
2014-06-03 21:44:18 +0100

    Initial Commit

---
+++ swcat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swcat
+//
+//  Created by Caius Durling on 03/06/2014.
+//  Copyright (c) 2014 Caius Durling. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
